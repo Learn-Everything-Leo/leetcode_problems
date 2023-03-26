ec7a45b86a0ffea84bbd31c1e7855783d14a6497
Leo Peng <pengzimaobuaa@163.com>
2023-03-26 13:23:12 +0800

    ✨ feat: add two optimized solution

--- check_inclusion.dart
+++ check_inclusion.dart
@@ -38,3 +38,107 @@ class Solution {
     return false;
   }
 }
+
+class Solution2 {
+  bool checkInclusion(String s1, String s2) {
+    int n = s1.length, m = s2.length;
+    if (n > m) {
+      return false;
+    }
+    Map<String, int> countMap = {};
+    for (int i = 0; i < n; i++) {
+      if (countMap.containsKey(s1[i])) {
+        countMap[s1[i]] = countMap[s1[i]]! + 1;
+      } else {
+        countMap[s1[i]] = 1;
+      }
+      if (countMap.containsKey(s2[i])) {
+        countMap[s2[i]] = countMap[s2[i]]! - 1;
+      } else {
+        countMap[s2[i]] = -1;
+      }
+    }
+
+    int diff = 0;
+    for (var count in countMap.values) {
+      if (count != 0) {
+        diff++;
+      }
+    }
+
+    if (diff == 0) {
+      return true;
+    }
+
+    for (int i = n; i < m; i++) {
+      String x = s2[i - n], y = s2[i];
+      if (x == y) {
+        continue;
+      }
+
+      if ((countMap[x] ?? 0) == 0) {
+        diff++;
+      }
+      if (countMap.containsKey(x)) {
+        countMap[x] = countMap[x]! + 1;
+      } else {
+        countMap[x] = 1;
+      }
+      if (countMap[x] == 0) {
+        diff--;
+      }
+
+      if ((countMap[y] ?? 0) == 0) {
+        diff++;
+      }
+      if (countMap.containsKey(y)) {
+        countMap[y] = countMap[y]! - 1;
+      } else {
+        countMap[y] = -1;
+      }
+      if (countMap[y] == 0) {
+        diff--;
+      }
+
+      if (diff == 0) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
+
+class Solution3 {
+  bool checkInclusion(String s1, String s2) {
+    int n = s1.length, m = s2.length;
+    if (n > m) {
+      return false;
+    }
+    Map<String, int> countMap = {};
+    s1.split('').forEach((element) {
+      if (countMap.containsKey(element)) {
+        countMap[element] = countMap[element]! - 1;
+      } else {
+        countMap[element] = -1;
+      }
+    });
+    int left = 0;
+    for (int right = 0; right < m; right++) {
+      String x = s2[right];
+      if (countMap.containsKey(x)) {
+        countMap[x] = countMap[x]! + 1;
+      } else {
+        countMap[x] = 1;
+      }
+      while (countMap[x]! > 0) {
+        countMap[s2[left]] = countMap[s2[left]]! - 1;
+        left++;
+      }
+      if (right - left + 1 == n) {
+        return true;
+      }
+    }
+    return false;
+  }
+}

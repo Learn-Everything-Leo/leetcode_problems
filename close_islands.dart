1bfeec9d4a50ba9e45cd2ef911467402c4a319d4
Leo Peng <pengzimaobuaa@163.com>
2023-06-18 23:27:10 +0800

    🐞 fix:  修复异常及删除冗余代码

--- close_islands.dart
+++ close_islands.dart
@@ -1,31 +1,27 @@
 class Solution {
   int closedIsland(List<List<int>> grid) {
-    List<int> map = [];
+    List<int> valueList = [];
     int value = 1;
     int n = grid.length, m = grid[0].length;
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < m; j++) {
         if (grid[i][j] == 0) {
           dfs(grid, i, j, ++value);
-          map.add(value);
+          valueList.add(value);
         }
         if (grid[i][j] == 1) {
           continue;
         }
         if (i == 0 || j == 0 || i == n - 1 || j == m - 1) {
-          map.remove(grid[i][j]);
+          valueList.remove(grid[i][j]);
         }
       }
     }
-    return map.length;
+    return valueList.length;
   }
 
   void dfs(List<List<int>> grid, int row, int col, int value) {
-    if (row < grid.length && col < grid[0].length) {
-      if (grid[row][col] == 1) {
-        return;
-      }
-
+    if (row >= 0 && col >= 0 && row < grid.length && col < grid[0].length) {
       if (grid[row][col] == 0) {
         grid[row][col] = value;
         dfs(grid, row + 1, col, value);

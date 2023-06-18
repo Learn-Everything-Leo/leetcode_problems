f02b9eee926d0dee18942e383771fc37af568587
Leo Peng <pengzimaobuaa@163.com>
2023-06-18 23:17:18 +0800

    ✨ feat: 封闭岛的个数

---
+++ close_islands.dart
@@ -0,0 +1,38 @@
+class Solution {
+  int closedIsland(List<List<int>> grid) {
+    List<int> map = [];
+    int value = 1;
+    int n = grid.length, m = grid[0].length;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        if (grid[i][j] == 0) {
+          dfs(grid, i, j, ++value);
+          map.add(value);
+        }
+        if (grid[i][j] == 1) {
+          continue;
+        }
+        if (i == 0 || j == 0 || i == n - 1 || j == m - 1) {
+          map.remove(grid[i][j]);
+        }
+      }
+    }
+    return map.length;
+  }
+
+  void dfs(List<List<int>> grid, int row, int col, int value) {
+    if (row < grid.length && col < grid[0].length) {
+      if (grid[row][col] == 1) {
+        return;
+      }
+
+      if (grid[row][col] == 0) {
+        grid[row][col] = value;
+        dfs(grid, row + 1, col, value);
+        dfs(grid, row, col + 1, value);
+        dfs(grid, row - 1, col, value);
+        dfs(grid, row, col - 1, value);
+      }
+    }
+  }
+}

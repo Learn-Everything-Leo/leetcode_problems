a3a483a07a8cee6aec9b86b5ad1f78f84ff07805
Leo Peng <pengzimaobuaa@163.com>
2023-07-02 00:51:44 +0800

    ✨ feat: 两数相加

---
+++ add_two_numbers.dart
@@ -0,0 +1,35 @@
+class ListNode {
+  int val;
+  ListNode? next;
+  ListNode([this.val = 0, this.next]);
+}
+
+class Solution {
+  ListNode? addTwoNumbers(ListNode? l1, ListNode? l2) {
+    return add(l1, l2, 0);
+  }
+
+  ListNode? add(ListNode? l1, ListNode? l2, int carryOver) {
+    if (l1 == null && l2 == null) {
+      return carryOver > 0 ? ListNode(carryOver, null) : null;
+    }
+    if (l1 == null) {
+      int value = carryOver + l2!.val;
+      if (value >= 10) {
+        return ListNode(value % 10, add(null, l2.next, 1));
+      } else {
+        return ListNode(value % 10, l2.next);
+      }
+    }
+    if (l2 == null) {
+      int value = carryOver + l1.val;
+      if (value >= 10) {
+        return ListNode(value % 10, add(l1.next, null, 1));
+      } else {
+        return ListNode(value % 10, l1.next);
+      }
+    }
+    int value = carryOver + l1.val + l2.val;
+    return ListNode(value % 10, add(l1.next, l2.next, value ~/ 10));
+  }
+}

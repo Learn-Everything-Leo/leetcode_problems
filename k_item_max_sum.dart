3e375b860b71c2178f300995d3a22426f9e553ad
Leo Peng <pengzimaobuaa@163.com>
2023-07-05 22:33:19 +0800

    ✨ feat: K件物品的最大和

---
+++ k_item_max_sum.dart
@@ -0,0 +1,11 @@
+class Solution {
+  int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+    if (k <= numOnes) {
+      return k;
+    }
+    if (k <= numOnes + numZeros) {
+      return numOnes;
+    }
+    return numOnes - (k - numOnes - numZeros);
+  }
+}

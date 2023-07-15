edfc132a04d45e1e3d9685daf37154cbe8e50581
Leo Peng <pengzimaobuaa@163.com>
2023-07-15 15:41:20 +0800

    ✨ feat: 性能优化

--- four_sum.dart
+++ four_sum.dart
@@ -5,8 +5,24 @@ class Solution {
     nums.sort();
     int first = 0;
     while (first < n - 3) {
+      if (nums[first] + nums[first + 1] + nums[first + 2] + nums[first + 3] >
+          target) {
+        break;
+      }
+      while (first < n - 3 &&
+          nums[first] + nums[n - 1] + nums[n - 2] + nums[n - 3] < target) {
+        first++;
+      }
       int start = first + 1;
       while (start < n - 2) {
+        if (nums[first] + nums[start] + nums[start + 1] + nums[start + 2] >
+            target) {
+          break;
+        }
+        while (start < n - 2 &&
+            nums[first] + nums[start] + nums[n - 1] + nums[n - 2] < target) {
+          start++;
+        }
         int left = start + 1, right = n - 1;
         while (left < right) {
           int sum = nums[first] + nums[start] + nums[left] + nums[right];

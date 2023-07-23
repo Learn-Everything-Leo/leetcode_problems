3a08bc4ebed7af33358476632caffced6d842248
Leo Peng <pengzimaobuaa@163.com>
2023-07-23 17:18:26 +0800

    ✨ feat: 接雨水

---
+++ trap.dart
@@ -0,0 +1,20 @@
+import 'dart:math';
+
+class Solution {
+  int trap(List<int> height) {
+    int n = height.length;
+    List<int> leftMax = List.generate(n, (index) => 0);
+    List<int> rightMax = List.generate(n, (index) => 0);
+    leftMax[0] = height[0];
+    rightMax[0] = height[n - 1];
+    for (int i = 1; i < n; i++) {
+      leftMax[i] = max(height[i], leftMax[i - 1]);
+      rightMax[i] = max(height[n - 1 - i], rightMax[i - 1]);
+    }
+    int res = 0;
+    for (int i = 1; i < n - 1; i++) {
+      res += max(0, min(leftMax[i - 1], rightMax[n - 1 - i - 1]) - height[i]);
+    }
+    return res;
+  }
+}

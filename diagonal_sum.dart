3cb3d5f50787e64a562db043e7c407e4f02eca8e
Leo Peng <pengzimaobuaa@163.com>
2023-08-11 23:24:57 +0800

    ✨ feat: #1572 矩阵对角线的和

---
+++ diagonal_sum.dart
@@ -0,0 +1,13 @@
+class Solution {
+  int diagonalSum(List<List<int>> mat) {
+    int n = mat.length;
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+      sum += mat[i][i];
+      if (i != n - 1 - i) {
+        sum += mat[i][n - 1 - i];
+      }
+    }
+    return sum;
+  }
+}

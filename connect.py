910e58ece62e0f3defb94e823e9177dceb99d486
Leobuaa <pengzimaobuaa@163.com>
2023-11-03 08:48:56 +0800

    ✨ feat: 每个节点的右侧节点

---
+++ connect.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return root
+        queue = deque([root])
+        while queue:
+            n = len(queue)
+            last = None
+            for _ in range(n):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                if last:
+                    last.next = node
+                last = node
+        return root
+        

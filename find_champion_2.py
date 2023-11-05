c1cfea9499875d378c10f7e379183bcf1ae36865
Leobuaa <pengzimaobuaa@163.com>
2023-11-05 11:23:27 +0800

    ✨ feat: 寻找冠军2

---
+++ find_champion_2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findChampion(self, n: int, edges: List[List[int]]) -> int:
+        arr = [i for i in range(n)]
+        for edge in edges:
+            for (_, v) in enumerate(arr):
+                if v == edge[1]:
+                    arr.remove(v)
+        if len(arr) == 1:
+            return arr[0]
+        else:
+            return -1
+        

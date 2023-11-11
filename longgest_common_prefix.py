31b1b149a14e74db2defaf596aba91a35c4a811b
Leobuaa <pengzimaobuaa@163.com>
2023-11-11 22:27:17 +0800

    ✨ feat:  最长公共子串

---
+++ longgest_common_prefix.py
@@ -0,0 +1,21 @@
+from ast import List
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        
+        prefix, count = strs[0], len(strs)
+        for i in range(1, count):
+            prefix = self.lcp(prefix, strs[i])
+            if not prefix:
+                break
+        
+        return prefix
+
+    def lcp(self, str1, str2):
+        length, index = min(len(str1), len(str2)), 0
+        while index < length and str1[index] == str2[index]:
+            index += 1
+        return str1[:index]

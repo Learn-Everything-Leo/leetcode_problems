51c323423777f9cb9fef9a3388ad015d049c895c
Leobuaa <pengzimaobuaa@163.com>
2023-11-14 08:36:05 +0800

    ✨ feat: 特殊位置

---
+++ num_special.py
@@ -0,0 +1,13 @@
+from ast import List
+
+
+class Solution:
+    def numSpecial(self, mat: List[List[int]]) -> int:
+        rows_sum = [sum(row) for row in mat]
+        cols_sum = [sum(col) for col in zip(*mat)]
+        res = 0
+        for i, row in enumerate(mat):
+            for j, x in enumerate(row):
+                if x == 1 and rows_sum[i] == 1 and cols_sum[j] == 1:
+                    res += 1
+        return res

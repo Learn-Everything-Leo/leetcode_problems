f4de50ba8c3fe32f8ca099499817b2c92e4d5fcd
Leo Peng <pengzimaobuaa@163.com>
2023-12-07 15:50:21 +0800

    ✨ feat: 2595 奇偶位数

---
+++ even_odd_bits.py
@@ -0,0 +1,12 @@
+from ast import List
+
+
+class Solution:
+    def evenOddBit(self, n: int) -> List[int]:
+        ans = [0, 0]
+        i = 0
+        while n:
+            ans[i] += n & 1
+            n >>= 1
+            i ^= 1
+        return ans

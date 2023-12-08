5f340663d930d2ed02f2b5532103c65f74c433b8
Leobuaa <pengzimaobuaa@163.com>
2023-12-08 22:27:07 +0800

    ✨ feat: 1078 Bigram 分词

---
+++ find_ocurrences.py
@@ -0,0 +1,7 @@
+from ast import List
+
+
+class Solution:
+    def findOcurrences(self, text: str, first: str, second: str) -> List[str]:
+        words = text.split()
+        return [words[i] for i in range(2, len(words)) if words[i - 2] == first and words[i - 1] == second]

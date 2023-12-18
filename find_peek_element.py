5f161bf3c8717a74ed176f79d92337404a539175
Leo Peng <pengzimaobuaa@163.com>
2023-12-18 11:54:13 +0800

    ✨ feat: 162 寻找峰值

---
+++ find_peek_element.py
@@ -0,0 +1,10 @@
+from ast import List
+
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        idx = 0
+        for i in range(1, len(nums)):
+            if nums[i] > nums[idx]:
+                idx = i
+        return idx

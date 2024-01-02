69694234e1eb8178776efdbfdb61f448f6ff164a
Leo Peng <pengzimaobuaa@163.com>
2024-01-02 11:04:08 +0800

    ✨ feat: 66 加1

---
+++ plus_one.py
@@ -0,0 +1,16 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        digits.reverse()
+        n = len(digits)
+        digits[0] += 1
+        for i in range(n):
+            if digits[i] >= 10:
+                digits[i] = digits[i] % 10
+                if (i != n-1):
+                    digits[i+1] += 1
+                else:
+                    digits.append(1)
+            else:
+                break
+        digits.reverse()
+        return digits

7bdd1717660da1e00f537e15baa90248cf78783b
Leobuaa <pengzimaobuaa@163.com>
2024-01-08 00:06:13 +0800

    ✨ feat: 447 回旋镖的数量

---
+++ number_of_boomerangs.py
@@ -0,0 +1,15 @@
+from ast import List
+from collections import defaultdict
+
+
+class Solution:
+    def numberOfBoomerangs(self, points: List[List[int]]) -> int:
+        ans = 0
+        for p in points:
+            cnt = defaultdict(int)
+            for q in points:
+                dis = (p[0] - q[0]) * (p[0] - q[0]) + (p[1] - q[1]) * (p[1] - q[1])
+                cnt[dis] += 1
+            for m in cnt.values():
+                ans += m * (m - 1)
+        return ans

4ffe2c2443b71d588cac99fe14202714c446903b
Leo Peng <pengzimaobuaa@163.com>
2024-01-17 09:48:50 +0800

    ✨ feat: 118 杨辉三角

---
+++ generate.py
@@ -0,0 +1,8 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = [[]] * numRows
+        for i in range(numRows):
+            res[i] = [1] * (i + 1)
+            for j in range(1, i):
+                res[i][j] = res[i - 1][j - 1] + res[i - 1][j]
+        return res

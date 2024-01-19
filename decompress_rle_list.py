994c829d1bf1c01bdd883a647a25d04d7f35a5c4
Leo Peng <pengzimaobuaa@163.com>
2024-01-19 09:43:33 +0800

    ✨ feat: 1313 解压缩编码列表

---
+++ decompress_rle_list.py
@@ -0,0 +1,9 @@
+from ast import List
+
+
+class Solution:
+    def decompressRLElist(self, nums: List[int]) -> List[int]:
+        ans = list()
+        for i in range(0, len(nums), 2):
+            ans.extend([nums[i + 1]] * nums[i])
+        return ans

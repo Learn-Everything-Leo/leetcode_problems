0f27bd2eb8872d088bd9756f3686b3567b1343d6
Leo Peng <pengzimaobuaa@163.com>
2024-01-31 10:05:40 +0800

    ✨ feat: 2670 找出不同元素数目差数组

---
+++ distinct_difference_array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def distinctDifferenceArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        sufCnt = [0] * (n + 1)
+        st = set()
+        for i in range(n - 1, 0, -1):
+            st.add(nums[i])
+            sufCnt[i] = len(st)
+        res = []
+        st.clear()
+        for i in range(n):
+            st.add(nums[i])
+            res.append(len(st) - sufCnt[i + 1])
+        return res

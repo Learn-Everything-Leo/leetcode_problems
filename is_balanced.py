366f6096388f65fae233312da5c3cd2bf793ca22
Leobuaa <pengzimaobuaa@163.com>
2024-02-03 10:07:49 +0800

    ✨ feat: 110 平衡二叉树

---
+++ is_balanced.py
@@ -0,0 +1,16 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        def height(root: TreeNode) -> int:
+            if not root:
+                return 0
+            return max(height(root.left), height(root.right)) + 1
+
+        if not root:
+            return True
+        return abs(height(root.left) - height(root.right)) <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right)

945a60b7348252a2319b0e553bf09235063b15bb
Leobuaa <pengzimaobuaa@163.com>
2024-02-11 09:49:37 +0800

    ✨ feat: 144 二叉树的前序遍历

---
+++ pre_order_traversal.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+from altair import List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        def dfs(root):
+            if root is None:
+                return
+            ans.append(root.val)
+            dfs(root.left)
+            dfs(root.right)
+
+        ans = []
+        dfs(root)
+        return ans

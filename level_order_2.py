1f35c9bf6215a5e7b092b834b52f31174dfde46b
Leobuaa <pengzimaobuaa@163.com>
2024-02-15 01:55:00 +0800

    ✨ feat: 107 二叉树的层序遍历

---
+++ level_order_2.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+from altair import List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+        levelOrder = list()
+        if not root:
+            return levelOrder
+        
+        q = collections.deque([root])
+        while q:
+            level = list()
+            size = len(q)
+            for _ in range(size):
+                node = q.popleft()
+                level.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            levelOrder.append(level)
+
+        return levelOrder[::-1]

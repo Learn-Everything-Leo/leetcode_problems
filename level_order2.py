e1f2504ca152cf5fd5540fd0b160e867f38a9c13
Leo Peng <pengzimaobuaa@163.com>
2024-02-17 16:06:13 +0800

    更新 429 N叉树的层序遍历

---
+++ level_order2.py
@@ -0,0 +1,28 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        if not root:
+            return []
+
+        ans = list()
+        q = deque([root])
+
+        while q:
+            cnt = len(q)
+            level = list()
+            for _ in range(cnt):
+                cur = q.popleft()
+                level.append(cur.val)
+                for child in cur.children:
+                    q.append(child)
+            ans.append(level)
+
+        return ans

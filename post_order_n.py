5fbd3e05b5c42c5f4f305e561257d27cc025cbb8
Leo Peng <pengzimaobuaa@163.com>
2024-02-19 09:38:32 +0800

    ✨ feat: 590 N叉树的后序遍历

---
+++ post_order_n.py
@@ -0,0 +1,17 @@
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        ans = []
+        def dfs(node: 'Node'):
+            if node is None:
+                return
+            for ch in node.children:
+                dfs(ch)
+            ans.append(node.val)
+        dfs(root)
+        return ans

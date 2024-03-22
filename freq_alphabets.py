6275cf9d5c447762ca4c88496d62035f4c7c1c64
Leo Peng <pengzimaobuaa@163.com>
2024-03-22 09:32:10 +0800

    ✨ feat: 1309 解码字母到整数映射

---
+++ freq_alphabets.py
@@ -0,0 +1,14 @@
+class Solution:
+    def freqAlphabets(self, s: str) -> str:
+        def get(st):
+            return chr(int(st) + 96)
+
+        i, ans = 0, ""
+        while i < len(s):
+            if i + 2 < len(s) and s[i + 2] == '#':
+                ans += get(s[i : i + 2])
+                i += 2
+            else:
+                ans += get(s[i])
+            i += 1
+        return ans

75f5c9afef3233876ca8f47f103e2ba69aab9d3c
Leo Peng <pengzimaobuaa@163.com>
2024-04-12 13:53:38 +0800

    ✨ feat: 2923 找到冠军

---
+++ find_champion_1.py
@@ -0,0 +1,10 @@
+from ast import List
+
+
+class Solution:
+    def findChampion(self, grid: List[List[int]]) -> int:
+        ans = 0
+        for i in range(len(grid)):
+            if grid[i][ans] == 1:
+                ans = i
+        return ans

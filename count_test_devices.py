e1c0325256f1522ac010dfd1e56d1e08f43e9f6a
Leo Peng <pengzimaobuaa@163.com>
2024-05-10 10:25:28 +0800

    ✨ feat: 2960 统计已测试设备

---
+++ count_test_devices.py
@@ -0,0 +1,7 @@
+class Solution:
+    def countTestedDevices(self, batteryPercentages: List[int]) -> int:
+        res = 0
+        for i in range(len(batteryPercentages)):
+            if batteryPercentages[i] > res:
+                res += 1
+        return res

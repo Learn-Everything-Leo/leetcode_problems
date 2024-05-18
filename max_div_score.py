5693910735cba8d06b909ca6746f6a95307d7767
Leo Peng <pengzimaobuaa@163.com>
2024-05-18 14:20:39 +0800

    ✨ feat: 2644 找出可整除性得分最大的整数

---
+++ max_div_score.py
@@ -0,0 +1,22 @@
+from ast import List
+from itertools import pairwise
+
+
+class Solution:
+    def maxDivScore(self, nums: List[int], divisors: List[int]) -> int:
+        nums.sort(reverse=True)
+        dup = sum(1 for x, y in pairwise(nums) if x == y)
+        divisors.sort()
+        max_cnt, ans = -1, 0
+        for d in divisors:
+            if (max_cnt - dup + 1) * d > nums[0]:
+                break
+            cnt = 0
+            for x in nums:
+                if x < d:
+                    break
+                if x % d == 0:
+                    cnt += 1
+            if cnt > max_cnt:
+                max_cnt, ans = cnt, d
+        return ans

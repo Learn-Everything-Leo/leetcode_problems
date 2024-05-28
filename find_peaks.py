1fcd2146972e040a0a1bb8e12df112c8d52fa363
Leo Peng <pengzimaobuaa@163.com>
2024-05-28 09:42:03 +0800

    ✨ feat: 2951 找出峰值

---
+++ find_peaks.py
@@ -0,0 +1,10 @@
+from ast import List
+
+
+class Solution:
+    def findPeaks(self, mountain: List[int]) -> List[int]:
+            res = []
+            for i in range(1, len(mountain) - 1):
+                if mountain[i - 1] < mountain[i] and mountain[i] > mountain[i + 1]:
+                    res.append(i)
+            return res

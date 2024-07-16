a2ec29deb65c0d572905b8c08d6ff2dd7271353f
Leo Peng <pengzimaobuaa@163.com>
2024-07-16 14:50:26 +0800

    ✨ feat: 2956 找到两个数组中的公共元素

---
+++ find_intersection_values.py
@@ -0,0 +1,13 @@
+from ast import List
+
+
+class Solution:
+    def findIntersectionValues(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        ans1, ans2 = 0, 0
+        for num in nums1:
+            if num in nums2:
+                ans1 += 1
+        for num in nums2:
+            if num in nums1:
+                ans2 += 1
+        return [ans1, ans2]

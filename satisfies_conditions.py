a4adce0e35400bd4678378db7fc5d01e66f92026
Leo Peng <pengzimaobuaa@163.com>
2024-08-29 10:06:21 +0800

    ✨ feat: 3142 判断矩阵是否满足条件

---
+++ satisfies_conditions.py
@@ -0,0 +1,9 @@
+class Solution:
+    def satisfiesConditions(self, grid: List[List[int]]) -> bool:
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if i + 1 < len(grid) and grid[i][j] != grid[i + 1][j]:
+                    return False
+                if j + 1 < len(grid[0]) and grid[i][j] == grid[i][j + 1]:
+                    return False
+        return True

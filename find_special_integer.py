2e1ee5d4bfbe59e034fa2fc7d0b75f106224eb47
Leo Peng <pengzimaobuaa@163.com>
2025-02-17 14:34:58 +0800

    ✨ feat: 1287. 有序数组中出现次数超过25%的元素

---
+++ find_special_integer.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def findSpecialInteger(self, arr: List[int]) -> int:
+        n = len(arr)
+        cur, cnt = arr[0], 0
+        for i in range(n):
+            if arr[i] == cur:
+                cnt += 1
+                if cnt * 4 > n:
+                    return cur
+            else:
+                cur, cnt = arr[i], 1
+        return -1

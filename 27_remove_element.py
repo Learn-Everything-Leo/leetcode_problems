2ff5a4069db66c1c635f031979ee26c5a01126a6
Leo Peng <pengzimaobuaa@163.com>
2025-11-10 16:07:15 +0800

    ✨ feat: 优化简洁移除元素

--- 27_remove_element.py
+++ 27_remove_element.py
@@ -1,19 +1,11 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         n = len(nums)
-        i, j = 0, n - 1
+        i, j = 0, n
         while i < j:
-            while i < j and nums[i] != val:
-                i += 1
-            while i < j and nums[j] == val:
-                j -= 1
-            if i < j:
-                nums[i], nums[j] = nums[j], nums[i]
-        if i < n:
             if nums[i] == val:
-                return i
+                nums[i] = nums[j - 1]
+                j -= 1
             else:
-                return i + 1
-        else:
-            return n
-         
+                i += 1
+        return i
